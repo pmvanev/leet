a4281e28126267b7d892be9c05a4db1e21fc3759
Philip Van Every <pvanevery@unm.edu>
2022-07-07 14:09:40 -0600

    clean up angle tests

--- self_problems/angle_between_clock_hands.py
+++ self_problems/angle_between_clock_hands.py
@@ -9,15 +9,17 @@ minutes_per_hour = 60.
 seconds_per_minute = 60.
 seconds_per_hour = 3600.
 deg_per_minute = 6.
-hour_hand_deg_per_hour = 30.
-deg_per_revolution = 360
+deg_per_revolution = 360.
+hours_per_revolution = 12.
+minutes_per_revolution = 60.
+seconds_per_revolution = 60.
 
 
 def hour_hand_deg(hour, minute, second):
-    deg_per_hour = hour_hand_deg_per_hour
+    deg_per_hour = 30.
     hour_offset = hour * deg_per_hour
     minute_offset = deg_per_hour * minute / minutes_per_hour
-    second_offset = deg_per_hour * second / seconds_per_hour
+    second_offset = deg_per_minute * second / seconds_per_hour
     return (hour_offset + minute_offset + second_offset) % deg_per_revolution
 
 
@@ -34,21 +36,46 @@ def small_angle_deg(angle_deg):
 
 
 def hand_angle_deg(hour, minute, second):
-    hhd = hour_hand_deg(hour, minute, second)
-    mhd = minute_hand_deg(minute, second)
+    _hour = hour % hours_per_revolution
+    _minute = minute % minutes_per_revolution
+    _second = second % seconds_per_revolution
+    hhd = hour_hand_deg(_hour, _minute, _second)
+    mhd = minute_hand_deg(_minute, _second)
     return small_angle_deg(abs(hhd - mhd))
 
 
 class TestTimeAngle(unittest.TestCase):
 
     def test_hand_angle_degrees(self):
-        self.assertEqual(hand_angle_deg(hour=12, minute=0, second=0), 0)
+        time = {'hour': 0, 'minute': 0, 'second': 0}
+        self.assertEqual(hand_angle_deg(**time), 0)
 
         time = {'hour': 12, 'minute': 15, 'second': 0}
-        _minute_hand_deg = 90
-        _hour_hand_deg = (time['minute'] / minutes_per_hour) * hour_hand_deg_per_hour
-        self.assertEqual(hand_angle_deg(**time),
-                         _minute_hand_deg - _hour_hand_deg)
+        # 15 min is .25 hour, so the hour hand should be at .25*30d
+        self.assertEqual(hand_angle_deg(**time), 90 - .25 * 30)
+
+        time = {'hour': 3, 'minute': 15, 'second': 0}
+        # 15 min is .25 hour, so the hour hand should be .25*30d past minute hand
+        self.assertEqual(hand_angle_deg(**time), .25 * 30)
+
+        time = {'hour': 6, 'minute': 30, 'second': 0}
+        # 30 min is .5 hour, so the hour hand should be .5*30d past minute hand
+        self.assertEqual(hand_angle_deg(**time), .5 * 30)
+
+        time = {'hour': 6, 'minute': 45, 'second': 0}
+        # 30 min is .5 hour, so the hour hand should be .75*30d past 6
+        self.assertEqual(hand_angle_deg(**time), 270 - (180 + .75 * 30))
+
+        time = {'hour': 11, 'minute': 59, 'second': 0}
+        # minute hand: 59/60 * 360
+        m = 59 / 60 * 360
+        h = (11 + 59 / 60) * 30
+        self.assertEqual(hand_angle_deg(**time), h - m)
+
+        time = {'hour': 12, 'minute': 00, 'second': 59}
+        m = 59 / 60 * 6
+        h = m / 60
+        self.assertAlmostEqual(hand_angle_deg(**time), m - h)
 
 
 if __name__ == '__main__':
