da18590293277d28f1b1a57dffdebe8a0bfce798
Philip Van Every <pvanevery@unm.edu>
2022-07-06 21:40:30 -0600

    initial commit

---
+++ self_problems/two_stack_list.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TwoStackList:
+    pass
+
+class TestTwoStackList(unittest.TestCase):
+
+    def test_push(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
