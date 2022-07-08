d7109d90b49f0d3155f0e1adca38e463fd0551e3
Philip Van Every <pvanevery@unm.edu>
2022-07-08 10:49:32 -0600

    finish int_log_2 tests

--- self_problems/int_log_2.py
+++ self_problems/int_log_2.py
@@ -22,23 +22,30 @@ class TestLog2(unittest.TestCase):
         self.assertEqual(lg(0), None)
         self.assertEqual(lg(-1), None)
 
-    def test_lg_powers_of_2(self):
+    def check_lg(self, x, y):
+        self.assertEqual(lg(x), y)
+        self.assertEqual(lg(x + 1), y)
+        self.assertEqual(lg(2 * x - 1), y)
+
+    def test_lg(self):
         self.assertEqual(lg(1), 0)
-        self.assertEqual(lg(2), 1)
-        self.assertEqual(lg(4), 2)
-        self.assertEqual(lg(8), 3)
-        self.assertEqual(lg(16), 4)
-        self.assertEqual(lg(32), 5)
-        self.assertEqual(lg(64), 6)
-        self.assertEqual(lg(128), 7)
-        self.assertEqual(lg(256), 8)
-        self.assertEqual(lg(512), 9)
-        self.assertEqual(lg(1024), 10)
-        self.assertEqual(lg(2048), 11)
-        self.assertEqual(lg(1024 * 1024), 20)
-        self.assertEqual(lg(1024 * 1024 * 1024), 30)
-        self.assertEqual(lg(1024 * 1024 * 1024 * 1024), 40)
-        self.assertEqual(lg(1024 * 1024 * 1024 * 1024 * 1024), 50)
+
+        self.check_lg(2, 1)
+        self.check_lg(4, 2)
+        self.check_lg(8, 3)
+        self.check_lg(16, 4)
+        self.check_lg(32, 5)
+        self.check_lg(64, 6)
+        self.check_lg(128, 7)
+        self.check_lg(256, 8)
+        self.check_lg(512, 9)
+        self.check_lg(1024, 10)
+        self.check_lg(2048, 11)
+        self.check_lg(4096, 12)
+        self.check_lg(1024 * 1024, 20)
+        self.check_lg(1024 * 1024 * 1024, 30)
+        self.check_lg(1024 * 1024 * 1024 * 1024, 40)
+        self.check_lg(1024 * 1024 * 1024 * 1024 * 1024, 50)
 
 
 if __name__ == '__main__':
